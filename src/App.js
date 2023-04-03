88562fe189930186855c92ff26349bbed6e9b945
athirakcdev <athirakc20@gmail.com>
2023-04-03 21:11:35 +0530

    fix:changed id back

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App=()=> {
       "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      "id": 6,
+      "id": 5,
       "title": "mens",
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
